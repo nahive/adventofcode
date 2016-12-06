777b835e5817ebb0fd7a11adab20de14895743c9
Szymon Maślanka <szymon.maslanka@intive.com>
2016-12-06 13:17:15 +0100

    day6 p2

---
+++ day6/p2.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+func process(input: [String]) -> String {
+  var eval = [[String:Int]](repeating: [String : Int](), count: input.first!.characters.count)
+  for str in input {
+    for (i,c) in str.characters.enumerated() {
+      eval[i][String(c)] = eval[i][String(c)] == nil ? 1 : eval[i][String(c)]! + 1
+    }
+  }
+  
+  return eval
+    .map{$0.sorted(by:{$0.1<$1.1}).first!}
+    .reduce("",{$0 + $1.0})
+}
+
+func read() -> [String] {
+  let fileURL = Bundle.main.url(forResource: "input", withExtension: "txt")
+  let content = try! String(contentsOf: fileURL!, encoding: .utf8)
+  return content.components(separatedBy: .newlines)
+}
+
+process(input: read())
