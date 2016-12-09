f470d7575bedc8f4cdf31d4ad0b83443085adca4
Szymon Maślanka <szymon.maslanka@intive.com>
2016-12-09 10:20:33 +0100

    day9

---
+++ day9/p2.swift
@@ -0,0 +1,37 @@
+import Foundation
+
+extension String {
+    func indexDistance(of character: Character) -> Int? {
+        guard let index = characters.index(of: character) else { return nil }
+        return distance(from: startIndex, to: index)
+    }
+}
+
+func process(input: [String]) -> Int {
+    func decompress(input: String) -> Int {
+        var input = input
+        var count = 0
+        for c in input.characters where c == "(" {
+            guard input != "" else { break }
+            count += input.indexDistance(of: "(")!
+            input = input.substring(from: input.range(of: "(")!.upperBound)
+            let mark = input[input.startIndex..<input.range(of: ")")!.lowerBound].components(separatedBy: "x").map { Int($0)! }
+            input = input.substring(from: input.range(of: ")")!.upperBound)
+            count += decompress(input: input.substring(to: input.index(input.startIndex, offsetBy: mark[0]))) * mark[1]
+            input = input.substring(from: input.index(input.startIndex, offsetBy: mark[0]))
+        }
+        count += input.characters.count
+        return count
+    }
+    
+    return input.reduce(0) { $0 + decompress(input: $1) }
+}
+
+
+func read() -> [String] {
+    let fileURL = Bundle.main.url(forResource: "input", withExtension: "txt")
+    let content = try! String(contentsOf: fileURL!, encoding: .utf8)
+    return content.components(separatedBy: .newlines)
+}
+
+process(input: read())
