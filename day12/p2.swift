3bfa29344ab1e939802a26a6e2fbb8b13b0bb1dc
Szymon Maślanka <szymon.maslanka@intive.com>
2016-12-14 10:43:46 +0100

    optimization

--- day12/p2.swift
+++ day12/p2.swift
@@ -10,11 +10,25 @@ public func process(input: [String]) -> Int {
         let ins = arr[0]
         switch ins {
         case "cpy": dict[arr[2]] = Int(arr[1]) ?? dict[arr[1]]!; i += 1
-        case "inc": dict[arr[1]]! += 1; i += 1
+        case "inc": 
+            let next = input[i+1].components(separatedBy: " ")
+            let nextnext = input[i+2].components(separatedBy: " ")
+            let ins = next[0]
+            if i + 1 < input.count &&
+            ins == "dec" &&
+            nextnext[0] == "jnz" &&
+            nextnext[1] == next[1] &&
+            nextnext[2] == "-2" {
+                dict[arr[1]]! += dict[next[1]]!
+                dict[next[1]] = 0
+                i += 3
+                continue
+            }
+            dict[arr[1]]! += 1; i += 1
         case "dec": dict[arr[1]]! -= 1; i += 1
         case "jnz": 
-        	let v = Int(arr[1]) ?? dict[arr[1]]!
-        	if v != 0 { i += Int(arr[2])! } else { i += 1 }
+            let v = Int(arr[1]) ?? dict[arr[1]]!
+            if v != 0 { i += Int(arr[2])! } else { i += 1 }
         default: i += 1
         }
     }
@@ -26,5 +40,6 @@ public func read(file: String) -> [String] {
 }
 
 let file = read(file: CommandLine.arguments[1])
+print(file)
 let p1 = process(input: file)
-print("p1: \(p2)")
+print("p2: \(p1)")
